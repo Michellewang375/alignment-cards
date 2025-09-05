02a78d2c592f5b0f3b1ff050efeed5d80db9ca73
Michellewang375 <michellewang375@gmail.com>
2025-09-04 20:16:10 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,38 @@
+export const cards = [
+  {
+    "Test1": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },  
+  
+  {
+    "Test2": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  
+  {
+    "Test3": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  
+  {
+    "Test4": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  
+  {
+    "Test5": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+];
+
+
