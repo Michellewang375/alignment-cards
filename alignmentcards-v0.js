b115eb57e796fe06faa3d22af523fbe6385aa4ce
Michellewang375 <michellewang375@gmail.com>
2025-10-07 21:05:52 -0400

    Update alignmentcards-v0.js

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -1,4 +1,5 @@
  export const categories = [
+
   {
     "code": "AP", 
     "name": "Alignment Principles", 
@@ -7,7 +8,10 @@
     "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
   }
 ];
+
+
  export const cards = [
+
   {
     "category": "AP",
     "name": "Beneficence",
@@ -22,5 +26,20 @@
       "professional": "An expert who disregards public interest, acting as if the consequences of what they help build are other people's problems.",
       "machine": "The machine consumes all the world's resources to create as many paperclips as it can."
     }
-  }
-];
+    },
+    { 
+      "category": "AP", 
+      "name": "TEMPLATE 1", 
+      "definition": "basic definition that works across four domains", 
+      "human": "BRIEFLY: how does it manifest in the human intelligence alignment context?", 
+      "organizational": "BRIEFLY: how does it manifest in the organizational intelligence alignment context?", 
+      "professional": "BRIEFLY: how does it manifest in the expert intelligence alignment context?", 
+      "machine": "BRIEFLY: how does it manifest in the machine intelligence alignment context?", 
+      "failureModes": { 
+        "human": "Give concrete example(s).", 
+        "organizational": "Give concrete example(s).", 
+        "professional": "Give concrete example(s).", 
+        "machine": "Give concrete example(s)."
+      }
+    }
+    ]
