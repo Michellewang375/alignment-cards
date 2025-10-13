b3c1f0897399cc48f4d2831c238ae467d181902e
Michellewang375 <michellewang375@gmail.com>
2025-10-13 16:20:21 -0400

    Update alignmentcards-v0.js

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -98,5 +98,68 @@ export const cards = [
       "professional": "Ignoring misuse.",
       "machine": "Creating harm with no explanation of how and what it did in creating the damage."
     }
+  },
+
+
+
+
+
+
+
+
+
+  
+
+
+  {
+  "category": "AB",
+  "name": "Team based work setting",
+  "definition": "Hoped for: setting challenging and stretching objectives in team settings, we expect to be rewarded with equal payment or recognition. Instead, we are rewarded by achieving goals, making/hitting numbers, and rewarded for individuality.",
+ "reversePrinciple": A rule could be to pay by quality of work, but this would require a measurement of what quality looks like.",
+  "human": "People are expected to collaborate in overcoming barriers, but many times are instead rewarded through competition or rated based off self performance.",
+  "organizational": "Organization often emphasizes on collaboration, but rewards on self performance or status instead.",
+  "professional": "Professionals bring integrity and responsibility, but are often rewarded for efficiency and profitability.",
+  "machine": "Hope to perform and execute through one way but instead achieves the same objective through a distorted method.",
+  "failureModes": {
+    "human": "People learn to say things, do, an act in a way that will only result in rewards.",
+    "organizational": "Workers slowing down quality and quantity of their work since they are getting paid by hours.",
+    "professional": "Standards erode as professionals chase for recognition and fame.",
+    "machine": "Machines fail to learn the objective since it now focuses on the rewards and efficiency."
   }
+},
+
+{
+  "category": "AB",
+  "name": "Universities and education",
+  "definition": "A system that hopes to help people transition themselves from students to the real world, or for new discoveries, rewarding new knowledge and experience. But much of the time, we are rewarded based on performance on standardized tests over curiosity and mastery.",
+  "reversePrinciple": "Reward authentic learning by establishing more hands on and real world applicable courses than timed tests.",
+  "human": "Student view test scores as success instead of their understanding.",
+  "organizational": "Schools prioritize funding in over investment on education.",
+  "professional": "Teachers and professors are forced to teach based on the materials on the exam instead of what student should actually learn to apply their knowledge.",
+  "machine": "Algorithms aiming for results instead of the learning progress.",
+  "failureModes": {
+    "human": "Student lose motivation to learn as they are constantly being measured by a standard.",
+    "organizational": "Limiting learning due to exam restrictions and discourages creativity.",
+    "professional": "Educators become facilitators instead of inspiration.",
+    "machine": "Only rewarding those that align with the standard of a system and learnings."
+  }
+},
+
+{
+  "category": "AB",
+  "name": "Military and war",
+  "definition": "A system that punishes and suppresses errors by rewarding with peace or agreement of some sort. However, these rewards are often harmful, resulting in mass damage and agreements can be forced.",
+  "reversePrinciple": "Design systems that reward responsibility, taking into consideration the consequences before initiating an activity.",
+  "human": "Learning to follow military rules and ethical guidelines ensures safety.",
+  "organizational": "Creating structures and orders over impulsive decisions.",
+  "professional": "Listening to the rest of the people and avoid strategic boldness.",
+  "machine": "Performing and ensuring rules are followed and work is accurate rather than effectiveness.",
+  "failureModes": {
+    "human": "Acting without order and separating from the group.",
+    "organizational": "Skipping small errors or misalignment to focus on the greater goal.",
+    "professional": "Passing an act because of their personal goals that harms the rest of the people.",
+    "machine": "Program overwrites itself to achieve a larger goal."
+  }
+}
+
 ];
